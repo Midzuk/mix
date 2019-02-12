8455c1179611dd66cea17509902c240f2fadb318
Midzuki <ucdmzk@icloud.com>
2019-02-12 14:54:45 +0900

    修正

--- src/Mix.hs
+++ src/Mix.hs
@@ -17,8 +17,25 @@ mix :: (Comonad w, Semigroup (w a))
 mix _ [] = []
 mix _ [x] = [x]
 mix f (x:xs) =
-  case partitionEithers $ (\x1 -> x1 `maybeToRight` f x x1) <$> xs of
-    (_, _) -> undefined
+  -- case partitionEithers $ (\x1 -> x1 `maybeToRight` f x x1) <$> xs of　-- :: ([a], [w a])
+  --  (_, _) -> undefined
   -- where
   --   g :: (a -> Maybe (w a)) -> a -> Validation [a] (w a)
   --   g h x = eitherToValidation . maybeToRight [x] $ h x
+
+mix :: Monoid a
+    => (a -> a -> Bool)
+    -> [a]
+    -> [a]
+
+mix _ [] = []
+mix _ [x] = [x]
+mix f (x:xs) =
+  let
+    (xs1, xs2) = f x `partition` xs
+  in
+    if null xs1
+    then x : mix xs
+    mix $ ((x <>) <$> xs1) <> xs2
+
+-- Cofree [] a
