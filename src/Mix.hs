856fae0aa70122da2935dc594d04bbeb7d961dfc
Midzuki <ucdmzk@icloud.com>
2019-02-15 15:35:50 +0900

    修正

--- src/Mix.hs
+++ src/Mix.hs
@@ -5,6 +5,7 @@
 -- {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE InstanceSigs         #-}
 -- {-# LANGUAGE AllowAmbiguousTypes  #-}
+{-# LANGUAGE ViewPatterns #-}
 
 
 module Mix
@@ -18,9 +19,51 @@ import Data.Either
 -- import Data.Either.Combinators
 -- import Data.Either.Validation
 import Data.Validation
--- import qualified Data.Set as S
+import qualified Data.Set as S
+import Data.List
 
-data Bowl = Bowl [Int] [Bowl] -- Cofree [] [Int]
+-- type Flour = S.Set Int
+-- type Bowl = Cofree [] Flour
+
+mix :: (Int -> Int -> Bool)
+    -> S.Set Int
+    -> [S.Set Int]
+mix f xs
+  | S.null xs = []
+  | otherwise = undefined
+  where
+    f1 :: (Int -> Int -> Bool)
+       -> S.Set Int
+       -> S.Set Int
+       -> Bool
+    f1 _ (S.null -> True) _ =
+      False
+    f1 g (S.splitAt 1 -> (x1, xs1)) xs2 =
+      (foldr (||) False . S.map (g x1) $ xs2) || f1 g xs1 xs2
+    
+    f2 :: (Int -> Int -> Bool)
+       -> S.Set Int
+       -> [Cofree [] (S.Set Int)]
+       -> [Cofree [] (S.Set Int)]
+    f2 g xs ys =
+      case partition (f1 g xs . extract) ys of
+        (ys1, ys2) -> -- ys1, ys2 :: [Cofree [] (S.Set Int)]
+          undefined
+      where
+        f21 :: S.Set Int
+            -> Cofree [] (S.Set Int)
+            -> Either (Cofree [] (S.Set Int)) (S.Set Int)
+        f21 xs ys
+          | f1 g xs (extract ys) || any (f21 xs <$> unwrap ys) =
+            Right . extract $ ys
+          | otherwise =
+            Left ys
+        
+        f22 :: [Either (Cofree [] (S.Set Int)) (S.Set Int)]
+            -> [Cofree [] (S.Set Int)]
+        f22 (Left x : xs) = [x]
+        f22 (Right _ : xs) = undefined 
+
 
 {-
 mix :: (Comonad w, Semigroup (w a))
